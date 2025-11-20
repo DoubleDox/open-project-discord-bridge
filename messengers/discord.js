f7d9c93ae4ccdc48cc5da18d5f2913bfa3cddb5e
DoubleDox <parax85@gmail.com>
2025-11-20 16:45:12 +0300

    schema and css improvements

--- messengers/discord.js
+++ messengers/discord.js
@@ -5,7 +5,20 @@ export function markup(message)
     return message.replace('<b>', '**').replace('</b>', '**');
 }
 
+export function prepareMessage(str)
+{
+    const message = { username: 'OP Bot', color: HEXToVBColor(b._embedded?.status?.color) };
+    message.fields = fields;
+}
+
 export function userLink(user)
 {
     return '<@' + (user[target] ?? user) +  '>';
+}
+
+
+function HEXToVBColor(rrggbb, start) {
+    let st = start ?? 0;
+    var bbggrr = rrggbb.substr(4 + st, 2) + rrggbb.substr(2 + st, 2) + rrggbb.substr(st, 2);
+    return parseInt(bbggrr, 16);
 }
